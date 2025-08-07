f8ff70a450a8c623eb8c69367abb37af3c199cb7
Luis Pulido <remote@luispulido.com>
2025-08-07 05:43:17 -0700

    added stockfish files

--- public/stockfish.js
+++
@@ -1,7 +0,0 @@
-// Stockfish JS engine v16.1.0
-// Downloaded from https://cdn.jsdelivr.net/npm/stockfish@16.1.0/stockfish.js
-// For full source, see: https://github.com/official-stockfish/Stockfish
-
-// ...existing code...
-// The actual Stockfish engine code is very large and minified.
-// For brevity, this is a placeholder. The real file will be the full stockfish.js from the CDN.
